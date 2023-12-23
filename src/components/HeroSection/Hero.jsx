db9865ea8bbfe57a278100d33b7df16922cf65be
Erickadikah <erickadikah2030@gmail.com>
2023-12-23 07:25:10 +0300

    update styled motion

--- src/components/HeroSection/Hero.jsx
+++ src/components/HeroSection/Hero.jsx
@@ -5,6 +5,8 @@ import masaaiImage from '../../../public/images/law1.jpg';
 import fourthImage from '../../../public/images/brief.jpeg';
 import fifthImage from '../../../public/images/7r.jpg';
 import secondImage from '../../../public/images/law1.jpg';
+import { GrNext } from "react-icons/gr";
+import { GrPrevious } from "react-icons/gr";
 
 const Hero = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -13,28 +15,28 @@ const Hero = () => {
     {
       image: masaaiImage,
       title: 'Muenda RoyFord & Company Advocates',
-      description: 'At Acuitus, we believe in the power of transformation. We stand as a beacon of innovation, inspiration, and progress.',
-      buttonLabel: 'Learn More',
+      description: 'Where Exelence Meets Justice',
+      buttonLabel: 'About Us',
       linkTo: '/about',
     },
     {
       image: fourthImage,
-      title: 'Activation',
-      description: 'From immersive events to captivating product launches, our Activation service transforms concepts into tangible moments that engage, inspire, and leave a lasting impact',
+      title: 'Arbitration',
+      description: 'We have represented clients in local and international arbitration tribunals including International Centre for Settlement of Investment Disputes (ICSID), the London Court of International Arbitration (LCIA) and the International Chamber of Commerce (ICC) in energy, financial services and construction sectors.',
       buttonLabel: 'Our Services',
       linkTo: '/services',
     },
     {
       image: fifthImage,
-      title: 'Trainings',
-      description: 'We are committed to offering innovative solutions that address a wide range of business challenges. As part of our dedication to sustainability and diversified expertise, we are proud to introduce our specialized farming training program',
+      title: 'Banking & Finance',
+      description: 'Our Banking & Finance practise area is well regarded for its expertise in advising in both contentious and non-contentious matters',
       buttonLabel: 'Our Services',
       linkTo: '/services',
     },
     {
       image: secondImage,
-      title: 'Research',
-      description: 'We provide insights that help you make informed decisions. Get a deeper understanding of your industry, enabling you to stay ahead of the curve and respond effectively to changing landscapes',
+      title: 'Assets Tracing & Recovery',
+      description: 'Our Asset Tracing & Recoveries practice area is a recognised leader in advising both contentious and noncontentious complex tracing and recovery of assets and in restructuring & insolvencies.',
       buttonLabel: 'Our Services',
       linkTo: '/services',
     }
@@ -53,59 +55,67 @@ const Hero = () => {
   }
 
   const handlePrev = () => {
-    setCurrentIndex((prevIndex) => (prevIndex - 1) % imagesData.length);
+    setCurrentIndex((prevIndex) => (prevIndex - 1 + imagesData.length) % imagesData.length);
   }
-
-  const overlayStyle = {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    width: '100%',
-    height: '100%',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)', // Adjust opacity as needed
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    textAlign: 'center',
-    color: 'white',
-    padding: '20px',
-  };
-
-  const titleStyle = {
-    fontSize: '4rem',
-    color: 'white',
-    fontWeight: 'bold',
-    lineHeight: '2',
-  };
-
-  const descriptionStyle = {
-    fontSize: '1.5rem',
-    color: 'white',
-    lineHeight: '1.5',
-    width: '50%',
-  };
 
   return (
     <div style={{ position: 'relative', height: '90vh' }}>
       <Image src={imagesData[currentIndex].image} alt="Hero Image" layout="fill" objectFit="cover" />
       <div style={overlayStyle}>
         <h1 style={titleStyle}>{imagesData[currentIndex].title}</h1>
-        <p style={descriptionStyle}
-        >{imagesData[currentIndex].description}</p>
-        {/*<Button>
-          <Link href={imagesData[currentIndex].linkTo}>
-            <a style={{ color: 'white' }}>{imagesData[currentIndex].buttonLabel}</a>
-          </Link>
-  </Button>*/}
-        <div className="d-flex justify-content-center mt-3">
-          <button className="btn btn-primary me-2 " onClick={handlePrev}>Prev</button>
-          <button className="btn btn-primary" onClick={handleNext}>Next</button>
-          </div>
+        <p style={descriptionStyle}>{imagesData[currentIndex].description}</p>
+        <button className="btn btn-primary mt-6" style={buttonStyle}>{imagesData[currentIndex].buttonLabel}</button>
+        <div className="flex justify-center mt-6 gap-96">
+        <button className="btn btn-primary m-2" onClick={handlePrev}>
+        <GrPrevious />
+        </button>
+        <button className="btn btn-primary" onClick={handleNext}>
+        <GrNext />
+        </button>
+        </div>
       </div>
     </div>
   );
 };
 
-export default Hero;
+const overlayStyle = {
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  width: '100%',
+  height: '100%',
+  backgroundColor: 'rgba(0, 0, 0, 0.5)', // Adjust opacity as needed
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  textAlign: 'center',
+  color: 'white',
+  padding: '20px',
+};
 
+const titleStyle = {
+  fontSize: '4rem',
+  color: 'white',
+  fontWeight: 'bold',
+  lineHeight: '2',
+  width: '50%',
+};
+
+const descriptionStyle = {
+  fontSize: '1.5rem',
+  color: 'white',
+  lineHeight: '1.5',
+  width: '50%',
+};
+
+const buttonStyle = {
+  fontSize: '1.5rem',
+  color: 'white',
+  lineHeight: '1.2',
+  width: '180px',
+  height: '60px',
+  backgroundColor: '#A65A2A',
+};
+
+export default Hero;
