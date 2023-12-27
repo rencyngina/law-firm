7afef77d960624be5fb6fd6c801d07fe9acea67c
Erickadikah <erickadikah2030@gmail.com>
2023-12-27 15:22:39 +0300

    update service #area

--- src/pages/estate-Administration.jsx
+++ src/pages/estate-Administration.jsx
@@ -13,7 +13,7 @@ const EstateAdministration = () => {
         <div
           className="h-52 w-full flex flex-col justify-center items-center"
           style={{
-            backgroundImage: `linear-gradient(rgba(21, 21, 21, 0.6), rgba(30, 29, 29, 0.6)), url(/images/call.jpg)`,
+            backgroundImage: `linear-gradient(rgba(21, 21, 21, 0.6), rgba(30, 29, 29, 0.6)), url(/images/property.jpg)`,
             backgroundPosition: "center",
             backgroundSize: "cover",
             backgroundRepeat: "no-repeat",
