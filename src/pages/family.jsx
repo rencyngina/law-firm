3fc7bfaceae0174d2581efb3636ef06f5749caf6
Erickadikah <erickadikah2030@gmail.com>
2024-01-05 18:32:49 +0300

    upadate family section

--- src/pages/family.jsx
+++ src/pages/family.jsx
@@ -18,6 +18,8 @@ const Family = () => {
           style={{
             backgroundImage: `linear-gradient(rgba(21, 21, 21, 0.6), rgba(30, 29, 29, 0.6)), url(/images/familylaw.jpg)`,
             backgroundPosition: "center",
+            objectFit: "contain",
+            objectFit: "cover",
             backgroundSize: "cover",
             backgroundRepeat: "no-repeat",
             backgroundAttachment: "fixed", // Modified this line for background attachment
