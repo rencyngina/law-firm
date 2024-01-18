5c754b6d55987a38554eed76f0e9c6d9e35d72bc
Erickadikah <erickadikah2030@gmail.com>
2024-01-18 20:17:36 +0300

    update postmark server function send message

---
+++ src/pages/api/message.js
@@ -0,0 +1,43 @@
+import { ServerClient } from 'postmark';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from 'react-toastify';
+
+export default async function messageRequest(req, res) {
+  const { name, email, phone, subject, message } = req.body;
+  console.log('Received data', { name, email, phone, subject, message });
+
+  if (req.method === 'POST') {
+    try {
+      const client = new ServerClient(process.env.POSTMARK_SERVER_TOKEN);
+      const messageBody = `
+        You have a new request for a call from ${name}.
+        Email: ${email}
+        Phone: ${phone}
+        Subject: ${subject}
+        Message: ${message}
+      `;
+
+      const response = await client.sendEmail({
+        From: process.env.SEND_FROM_EMAIL,
+        To: process.env.SEND_TO_EMAIL,
+        Subject: 'New call request',
+        TextBody: messageBody,
+      });
+
+      if (response.Message !== undefined && response.Message !== null) {
+        toast.success('Message sent');
+        return res.status(200).json({ message: 'Message sent' });
+      } else {
+        toast.error('Message not sent');
+        return res.status(400).json({ message: 'Message not sent' });
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      toast.error('Internal server error');
+      return res.status(500).json({ message: 'Internal server error' });
+    }
+  } else {
+    res.setHeader('Allow', ['POST']);
+    return res.status(405).json({ message: `Method ${req.method} not allowed` });
+  }
+}
