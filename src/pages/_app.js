a241846ec0aa365c5932fc3b0e27c3f00b96227d
Erickadikah <erickadikah2030@gmail.com>
2024-01-28 22:42:46 +0300

    update

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -9,7 +9,7 @@ function MyApp({ Component, pageProps }) {
         <meta name="description" content="Next.js TailwindCSS Starter Template" />
         <meta name="keywords" content="nextjs, tailwindcss, starter, template" />
         <title>Mwenda RoyFord & Company Advocates</title>
-        <meta name="description" content="Unlock legal excellence with Rofordlaw, where highly skilled professionals, graduates of Kenya's prestigious Law School, University of Nairobi, stand ready to guide you. Trust in our expertise for unparalleled legal services. Contact us today for a personalized consultation and embark on a journey to legal success." />
+        <meta name="description" content="Unlock legal excellence with Royfordlaw, where highly skilled professionals, graduates of Kenya's prestigious Law School, University of Nairobi, stand ready to guide you. Trust in our expertise for unparalleled legal services. Contact us today for a personalized consultation and embark on a journey to legal success." />
       </Head>
       <Component {...pageProps} />
     </>
