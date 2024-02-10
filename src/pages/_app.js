73f8d8e99a0a6b9f969e58747e655e28d30daf63
Erickadikah <erickadikah2030@gmail.com>
2024-02-10 14:20:52 +0300

    update

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,49 +2,89 @@ import Head from 'next/head';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
+  // Metadata object containing all the metadata for the webpage
+  const metadata = {
+    title: "Mwenda RoyFord & Company Advocates - Legal Excellence",
+    description: "Unlock legal excellence with Royfordlaw. Highly skilled professionals from Kenya's prestigious Law Schools, ready to guide you. Trust in our expertise for unparalleled legal services.",
+    keywords: "Royfordlaw, legal excellence, advocates, legal services, law, royfordlaw.com, royfordlaw, mwenda royford, law firm",
+    author: "Erick Adikah",
+    portfolio: {
+      name: "Erick Adikah",
+      sameAs: [
+        "https://www.linkedin.com/in/erickadikah",
+        "https://github.com/erickadikah",
+        "https://my-portfolio-sand-ten-81.vercel.app/"
+      ]
+    },
+    themeColor: "#1B2432",
+    favicon: "/images/favicon.ico",
+    robots: "index, follow, visit:daily",
+    og: {
+      title: "Mwenda RoyFord & Company Advocates - Legal Excellence",
+      description: "Unlock legal excellence with Royfordlaw. Highly skilled professionals from Kenya's prestigious Law School, University of Nairobi, ready to guide you. Trust in our expertise for unparalleled legal services.",
+      image: "/MWENDA ROYFORD LOGO-03 (1).png",
+      url: "https://www.royfordlaw.com/",
+      type: "website"
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: "Mwenda RoyFord & Company Advocates - Legal Excellence",
+      description: "Unlock legal excellence with Royfordlaw. Highly skilled professionals from Kenya's prestigious Law School, University of Nairobi, ready to guide you. Trust in our expertise for unparalleled legal services.",
+      image: "/images/favicon.ico",
+      site: "@royford_law"
+    }
+  };
+
   return (
     <>
       <Head>
         {/* Viewport Meta Tag */}
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        {/* Unique and Relevant Title Tag with Keywords */}
-        <title>Mwenda RoyFord & Company Advocates - Legal Excellence</title>
+        
+        {/* Title Tag */}
+        <title>{metadata.title}</title>
 
-        {/* Unique and Descriptive Meta Descriptions */}
-        <meta
-          name="description"
-          content="Unlock legal excellence with Royfordlaw. Highly skilled professionals from Kenya's prestigious Law Schools, ready to guide you. Trust in our expertise for unparalleled legal services."
-        />
+        {/* Meta Description */}
+        <meta name="description" content={metadata.description} />
 
         {/* Keywords Meta Tag */}
-        <meta name="keywords" content="Royfordlaw, legal excellence, advocates, legal services, law, royfordlaw.com, royfordlaw, mwenda royford, law firm" />
+        <meta name="keywords" content={metadata.keywords} />
 
         {/* Favicon Link */}
-        <link rel="icon" href="/images/favicon.ico" />
+        <link rel="icon" href={metadata.favicon} />
+        
         {/* Author Meta Tag */}
-        <meta name="author" content="Erick Adikah" />
+        <meta name="author" content={metadata.author} />
+
+        {/* Schema.org Markup for Portfolio */}
+        <script type="application/ld+json">
+          {JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "Person",
+            name: metadata.portfolio.name,
+            sameAs: metadata.portfolio.sameAs
+          })}
+        </script>
 
         {/* Robots Meta Tag */}
-        <meta name="robots" content="index, follow" />
+        <meta name="robots" content={metadata.robots} />
 
         {/* Theme Color Meta Tag */}
-        <meta name="theme-color" content="#1B2432" />
-        <meta name="royfordlaw" content="royfordlaw" />
-        <meta name="web application" content="rofordlaw.com" />
+        <meta name="theme-color" content={metadata.themeColor} />
 
         {/* Open Graph Meta Tags for Social Media */}
-        <meta property="og:title" content="Mwenda RoyFord & Company Advocates - Legal Excellence" />
-        <meta property="og:description" content="Unlock legal excellence with Royfordlaw. Highly skilled professionals from Kenya's prestigious Law School, University of Nairobi, ready to guide you. Trust in our expertise for unparalleled legal services." />
-        <meta property="og:image" content="/MWENDA ROYFORD LOGO-03 (1).png" />
-        <meta property="og:url" content="https://www.royfordlaw.com/" />
-        <meta property="og:type" content="website" />
+        <meta property="og:title" content={metadata.og.title} />
+        <meta property="og:description" content={metadata.og.description} />
+        <meta property="og:image" content={metadata.og.image} />
+        <meta property="og:url" content={metadata.og.url} />
+        <meta property="og:type" content={metadata.og.type} />
 
         {/* Twitter Meta Tags */}
-        <meta name="twitter:card" content="/images/favicon.ico" />
-        <meta name="twitter:title" content="Mwenda RoyFord & Company Advocates - Legal Excellence" />
-        <meta name="twitter:description" content="Unlock legal excellence with Royfordlaw. Highly skilled professionals from Kenya's prestigious Law School, University of Nairobi, ready to guide you. Trust in our expertise for unparalleled legal services." />
-        <meta name="twitter:image" content="/images/favicon.ico" />
-        <meta name="twitter:site" content="@royford_law" />
+        <meta name="twitter:card" content={metadata.twitter.card} />
+        <meta name="twitter:title" content={metadata.twitter.title} />
+        <meta name="twitter:description" content={metadata.twitter.description} />
+        <meta name="twitter:image" content={metadata.twitter.image} />
+        <meta name="twitter:site" content={metadata.twitter.site} />
       </Head>
       
       {/* Main Component */}
